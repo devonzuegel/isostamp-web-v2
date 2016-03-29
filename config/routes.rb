a1f4b1d873623246c8762a804ff8b2fa9d5f591b
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-03-28 19:08:41 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   root to: 'visitors#index'
+
   get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
-  get '/auth/failure' => 'sessions#failure'
+  get '/signin'                  => 'sessions#new',     as: :signin
+  get '/signout'                 => 'sessions#destroy', as: :signout
+  get '/auth/failure'            => 'sessions#failure'
 end
