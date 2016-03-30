f5f6ed25ad94a0820e4222f03b435306a6b3ea7f
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-03-30 16:13:26 -0700

    Add public/uploads/ to gitignore

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -4,7 +4,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "#{model.class.to_s.pluralize.underscore}/#{model.id}"
+    "uploads/#{model.class.to_s.pluralize.underscore}/#{model.id}"
   end
 
   def extension_white_list
