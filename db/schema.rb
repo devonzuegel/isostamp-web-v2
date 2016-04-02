451962cd16cc94538e41a062a38eb70ac7b9b059
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-01 20:24:07 -0700

    Remove format from documents

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20160330055751) do
   create_table "documents", force: :cascade do |t|
     t.string   "name"
     t.string   "attachment"
-    t.string   "format"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
