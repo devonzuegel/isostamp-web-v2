8d35fa6b5de0ea01248fe5acb4a0dddb448e9571
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-01 20:23:38 -0700

    Remove format from documents

--- db/migrate/20160330055751_create_documents.rb
+++ db/migrate/20160330055751_create_documents.rb
@@ -3,7 +3,6 @@ class CreateDocuments < ActiveRecord::Migration
     create_table :documents do |t|
       t.string     :name
       t.string     :attachment
-      t.string     :format
       t.references :user
 
       t.timestamps null: false
