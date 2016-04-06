987288637e931c06a15338c6a215e0a0eb6a8473
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-06 00:35:02 -0700

    Add admin column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330055751) do
+ActiveRecord::Schema.define(version: 20160406072952) do
 
   create_table "documents", force: :cascade do |t|
     t.string   "attachment"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20160330055751) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "admin",      default: false
   end
 
 end
