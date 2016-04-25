36ed8cad50cd885e1c730001ad3e3423c2db6db9
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-24 19:08:28 -0700

    Add filetype validations for mass spec data vs params

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,9 +4,30 @@ class Document < ActiveRecord::Base
 
   # Uploaders
   mount_uploader :attachment, MzxmlUploader
-  enum kind: { 'Mass Spec Data': 0, 'Params': 1 }
+
+  validates_presence_of :kind
+  enum kind: { 'Mass Spec Data': 0, 'Parameters': 1 }
+
+  EXTENSION_WHITE_LIST = %w(mzXML txt)
+  FILETYPE_TO_KIND_MAP = Hash[EXTENSION_WHITE_LIST.zip(self.kinds.keys)]
+
+  self.kinds.keys.zip(%w(.mzXML .txt)).to_h
+
+  validate :kind_matches_filetype
 
   def filename
     File.basename(attachment.path || '')
+  end
+
+  private
+
+  def kind_matches_filetype
+    ext = File.extname(attachment.path).sub('.', '')
+    return if kind == FILETYPE_TO_KIND_MAP[ext]
+
+    expected_ext = FILETYPE_TO_KIND_MAP.invert[kind]
+    msg = "must have the .#{expected_ext} filetype. If you meant to upload " +
+          "#{FILETYPE_TO_KIND_MAP[ext].downcase}, please say so in the form."
+    errors.add(:"#{kind}", msg)
   end
 end
