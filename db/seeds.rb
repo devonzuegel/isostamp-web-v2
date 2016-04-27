f7f3c3c3f9c7acc10b7fd66c6640d3056aa41e07
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-26 21:04:27 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-400.times do
-  User.create(
-    name:       Faker::Name.name,
-    provider:   'facebook',
-    created_at: Faker::Time.between(1.year.ago, 1.minute.ago)
-  )
-end
+# 400.times do
+#   User.create(
+#     name:       Faker::Name.name,
+#     provider:   'facebook',
+#     created_at: Faker::Time.between(1.year.ago, 1.minute.ago)
+#   )
+# end
 
-400.times do
-  user = User.order('RANDOM()').first
-  # Document.create(
-  #   user:         user,
-  #   created_at:   Faker::Time.between(user.created_at, 1.minute.ago)
-  # )
-end
+# 400.times do
+#   user = User.order('RANDOM()').first
+#   # Document.create(
+#   #   user:         user,
+#   #   created_at:   Faker::Time.between(user.created_at, 1.minute.ago)
+#   # )
+# end
