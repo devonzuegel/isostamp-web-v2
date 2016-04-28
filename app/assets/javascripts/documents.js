2600e42f4e6d49ee6136c6477edee05ab99c6a5e
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-04-28 15:57:23 -0700

    Frontend file upload looks good, including progress bar

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,11 +1,35 @@
 $(function() {
-  $('#s3_uploader').S3Uploader(
-    {
-      remove_completed_progress_bar: false,
-      progress_bar_target: $('#uploads_container')
-    }
-  );
-  $('#s3_uploader').bind('s3_upload_failed', function(e, content) {
-    return alert(content.filename + ' failed to upload');
+  $('#s3_uploader').S3Uploader({
+    allow_multiple_files:           false,
+    progress_bar_target:            $('#uploads-progress-bar'),
+    click_submit_target:            $('#submit-upload'),
+    remove_completed_progress_bar:  false
+  });
+
+  $('#s3_uploader').bind('s3_upload_failed', function (e, content) {
+    alert(content.filename + ' failed to upload');
+  });
+
+  $('#s3_uploader').bind('s3_uploads_start', function (e) {
+    $('#uploads-progress').show();
+  });
+
+  $('#s3_uploader').bind('s3_uploads_complete', function (e) {
+    setTimeout(function (){ location.reload(); }, 1000);
+  });
+
+  $('#uploads-progress-bar').bind('DOMSubtreeModified', function (e) {
+    var num_progress_bars = $('#uploads-progress-bar').children().length;
+    console.log("num_progress_bars = " + num_progress_bars);
+    if (num_progress_bars > 1) {
+      console.log($('#uploads-progress-bar').find('.upload:first'));
+      $('#uploads-progress-bar').find('.upload:first').remove();
+    };
+  });
+
+  $('#filename').bind('DOMSubtreeModified', function (e) {
+    if ($('#filename').text())
+      $('#submit-upload').prop('disabled', false);
   });
 });
+
