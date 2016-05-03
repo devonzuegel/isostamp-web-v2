e799c54458df496a1c1be0866522dfb931787769
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-03 13:42:15 -0700

    Add helper methods to TagfinderExecutable
    Add helper methods to TagfinderExecutable
    
    Fix view issue

--- app/models/tagfinder_execution.rb
+++ app/models/tagfinder_execution.rb
@@ -45,6 +45,26 @@ class TagfinderExecution < ActiveRecord::Base
     puts '------------------------------------------'.black
   end
 
+  def data_file_removed?
+    data_file.nil? && !data_file_id.nil?
+  end
+
+  def data_file_url
+    data_file.direct_upload_url
+  end
+
+  def data_file_name
+    data_file.upload_file_name
+  end
+
+  def used_default_params?
+    params_file_id.nil?
+  end
+
+  def params_file_removed?
+    !used_default_params? && params_file.nil?
+  end
+
   private
 
   def executable
