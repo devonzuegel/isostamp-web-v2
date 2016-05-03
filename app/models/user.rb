53b5a7bbc9fa3b4a1bea5feba8cfc0dd7fb9b3ed
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-02 22:39:08 -0700

    Add email to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ActiveRecord::Base
   has_many :sessions, class_name: 'Session'
 
   def self.create_with_omniauth(auth)
+    ap auth
     create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
+      user.provider = auth.fetch('provider')
+      user.uid      = auth.fetch('uid')
       if auth['info']
-         user.name = auth['info']['name'] || ""
+        user.name  = auth['info'].fetch('name')  || ''
+        user.email = auth['info'].fetch('email') || ''
       end
     end
   end
