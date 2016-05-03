6ec8b54aedd7cc79d3dbce7c840710b0e6770777
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-05-03 01:00:46 -0700

    Fix execution show view
    Fix execution show view

--- app/jobs/run_execution.rb
+++ app/jobs/run_execution.rb
@@ -3,7 +3,6 @@ require 'open3'
 class RunExecution < Que::Job
   def run(te_id)
     execution = TagfinderExecution.find(te_id)
-    ap execution
     ActiveRecord::Base.transaction do
         execution.run
         destroy
