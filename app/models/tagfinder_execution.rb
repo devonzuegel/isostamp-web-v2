3d070db34c02d5b4bf8e41e6e2a95286c5fc853d
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-06-10 13:57:28 -0700

    Add TagfinderExecutionDecorator specs
    Add TagfinderExecutionDecorator specs

--- app/models/tagfinder_execution.rb
+++ app/models/tagfinder_execution.rb
@@ -8,7 +8,6 @@ class TagfinderExecution < ActiveRecord::Base
   def run
     puts '=========================================='.black
     puts '=========================================='.black
-    @build_result = ''
     successful = shell.run("#{executable} #{tmp_filepath};")
     persist_results(successful)
     remove_tmp_file
@@ -37,26 +36,11 @@ class TagfinderExecution < ActiveRecord::Base
   end
 
   def download_tmp_file
-    stdin, stdout, stderr = Open3.popen3("wget #{data_file_url} -O #{@tmp_filepath};")
-
-    @build_result << "\nDownloading to '#{@tmp_filepath}':\n"
-    stdout.each_line { |line| @build_result << line }
-    @build_result <<  "\n\n"
-    stderr.each_line { |line| @build_result << line }
-    @build_result <<  "\n\n"
+    shell.run("wget #{data_file_url} -O #{@tmp_filepath};")
   end
 
   def print_contents_of_file
     shell.run("cat #{@tmp_filepath};")
-    @build_result << "\n\n------ CONTENTS OF '#{@tmp_filepath}': -----------\n\n"
-    @build_result << shell.outputs
-    @build_result << shell.errors
-    stdin, stdout, stderr = Open3.popen3("cat #{@tmp_filepath};")
-
-    stdout.each_line { |line| print line.green; @build_result << line }
-    @build_result <<  "\n\n"
-    stderr.each_line { |line| print line.red;   @build_result << line }
-    @build_result <<  "\n\n"
   end
 
   def remove_tmp_file
@@ -65,7 +49,10 @@ class TagfinderExecution < ActiveRecord::Base
   end
 
   def persist_results(successful)
-    update_attributes(result: @built_result, success: successful)
+    result  = "OUTPUTS = #{JSON.pretty_generate(shell.output.as_json)}"
+    result += "\n\n-------------------------------------------------\n\n"
+    result += "ERRORS  = #{JSON.pretty_generate(shell.errors.as_json)}"
+    update_attributes(result: result, success: successful)
     puts 'ATTRIBUTES PERSISTED'.black
     puts '=========================================='.black
   end
--- spec/decorators/tagfinder_execution_decorator_spec.rb
+++ spec/decorators/tagfinder_execution_decorator_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe TagfinderExecutionDecorator do
+  describe 'status' do
+    before { Document.any_instance.stub(:set_upload_attributes).and_return(true) }
+
+    it 'should be decorated with the expected decorator' do
+      @te = build(:tagfinder_execution).decorate
+      @te.should be_decorated_with TagfinderExecutionDecorator
+    end
+
+    it 'should be "Still running..." when success is not yet defined' do
+      @te = build(:tagfinder_execution).decorate
+      expect(@te.status).to eq("Still running...")
+    end
+
+    it 'should be "Success!" when success is true' do
+      @te = create(:tagfinder_execution, success: true).decorate
+      expect(@te.status).to eq("Success!")
+    end
+
+    it 'should be "Failure" when success is false' do
+      @te = create(:tagfinder_execution, success: false).decorate
+      expect(@te.status).to eq("Failure")
+    end
+  end
 end
