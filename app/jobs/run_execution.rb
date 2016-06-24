b43e7c696658ab0ee59705f2b07cfda12b011cf9
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-06-23 17:39:18 -0700

    Remove ActiveRecord::Base.transaction from RunExecution

--- app/jobs/run_execution.rb
+++ app/jobs/run_execution.rb
@@ -4,21 +4,22 @@ class RunExecution < Que::Job
   def run(te_id)
     execution = TagfinderExecution.find(te_id)
 
-    ActiveRecord::Base.transaction do
-      execution.log("Running execution #{te_id}...")
-      execution.run
+    execution.log("Running execution #{te_id}...")
 
-      if execution.successful?
-        execution.log('SUCCESS!!!!!!!!!!')
-        execution.log("Uploading results files for #{te_id}...")
-        UploadResultsFilesToS3.enqueue(te_id)
-      else
-        execution.log("FAILED results files for #{te_id}...")
-      end
+    # ActiveRecord::Base.transaction do
+    execution.run
 
-      execution.log("Sending email for #{te_id}...")
-      SendResultsEmail.enqueue(te_id, run_at: 10.seconds.from_now)
-      destroy
+    if execution.successful?
+      execution.log('SUCCESS!!!!!!!!!!')
+      execution.log("Uploading results files for #{te_id}...")
+      UploadResultsFilesToS3.enqueue(te_id)
+    else
+      execution.log("FAILED results files for #{te_id}...")
     end
+
+    execution.log("Sending email for #{te_id}...")
+    SendResultsEmail.enqueue(te_id, run_at: 10.seconds.from_now)
+    destroy
+    # end
   end
 end
