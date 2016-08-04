cd9df2ffaedd31b29ce64ef14eaeb1fc90414a60
Devon Kristine Zuegel <devonzuegel@gmail.com>
2016-08-03 23:00:30 -0700

    Add MAX_NUM_ATTEMPTS to RunExecution

--- app/jobs/run_execution.rb
+++ app/jobs/run_execution.rb
@@ -1,19 +1,24 @@
 class RunExecution < Que::Job
-  @retry_interval = 5
+  MAX_NUM_ATTEMPTS = 5
+  @retry_interval  = 5
 
   def run(te_id)
     execution = TagfinderExecution.find(te_id)
-    execution.increment!(:num_attempts)
-    execution.log("Beginning execution #{te_id} (Attempt ##{execution.num_attempts})")
+    if execution.num_attempts >= MAX_NUM_ATTEMPTS
+      execution.update_attributes(success: false)
+    else
+      execution.increment!(:num_attempts)
+      execution.log("Beginning execution #{te_id} (Attempt ##{execution.num_attempts})")
 
-    unless execution.success.nil?
-      execution.log("Clearing 'success' attribute on execution ##{execution.id}")
-      execution.update_attributes(success: nil)
+      unless execution.success.nil?
+        execution.log("Clearing 'success' attribute on execution ##{execution.id}")
+        execution.update_attributes(success: nil)
+      end
+
+      execution.run
+      execution.log("Sending email for #{te_id}...")
+      SendResultsEmail.enqueue(te_id, run_at: 10.seconds.from_now)
     end
-
-    execution.run
-    execution.log("Sending email for #{te_id}...")
-    SendResultsEmail.enqueue(te_id, run_at: 10.seconds.from_now)
     destroy
   end
 end
