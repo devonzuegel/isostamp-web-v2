b9de144d98b519429a8fa3cd03d0a02635448912
devon <devon@affirm.com>
2017-06-15 22:59:06 -0700

    Clean up deploy_production script

--- deploy_production.rb
+++ deploy_production.rb
@@ -10,12 +10,12 @@ end
 
 if permission_granted?('Promote `isostamp-staging` (staging) to `isostamp` (production)?')
   puts 'Promoting...'
-  puts `/usr/local/heroku/bin/heroku pipelines:promote --app isostamp-staging`
+  puts `heroku pipelines:promote --app isostamp-staging`
   puts 'Migrating...'
-  puts `/usr/local/heroku/bin/heroku run rake db:migrate --app isostamp`
+  puts `heroku run rake db:migrate --app isostamp`
 end
 
 if permission_granted?('Open heroku logs?')
   puts 'Opening heroku logs...'
-  exec '/usr/local/heroku/bin/heroku logs -t --app isostamp'
+  exec 'heroku logs -t --app isostamp'
 end
